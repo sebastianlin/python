bd68097f104847aeed4048fb64915a402fa4788b
Sebastian Lin <mickeylin75@gmail.com>
2014-04-30 14:54:16 +0800

    Check in a temporary script and modify readme.

---
+++ batch_modify_config/batch_modify_config.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+
+import os, glob, subprocess
+
+#for file in glob.glob('/home/mark/RTK_workshop/nike/svn/DTH_permission/nike_scpu/linux-2.6.34/config.*'):
+	file2 = open(file)
+	while 1:
+		line = file2.readline()
+		if not line: break
+		print line,
+		line.replace("", )
+#		subprocess.call(["echo", file])
+	file2.close()
+
+
+
+
+
+
