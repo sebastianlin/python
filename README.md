861ae5e21f33c2094125edfdc600d8d88c496f48
Sebastian Lin <mickeylin75@gmail.com>
2014-05-05 15:56:08 +0800

    ...

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 python
 ======
 
-Python scripts will be here.
+This is a script that will be used to modify many (kernel or uclibc) configs in one time.
+
+
--- batch_modify_config/batch_modify_config.py
+++ batch_modify_config/batch_modify_config.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python
 
 
-import os, glob, subprocess
+import os, sys, glob, subprocess, tempfile
 
-#for file in glob.glob('/home/mark/RTK_workshop/nike/svn/DTH_permission/nike_scpu/linux-2.6.34/config.*'):
+replace_str_array = [["CONFIG_CMDLINE_BOOL", "CONFIG_CMDLINE_BOOL=y", "# CONFIG_CMDLINE_BOOL is not set" ]]
+
+file4 = open(tempfile.mkstemp()[1], "rw")
+
+
+for file in glob.glob('/home/mark/RTK_workshop/nike/svn/DTH_permission/nike_scpu/linux-2.6.34/config.develop.avhdd.nike.scpu.secure.nxx.nand.old'):
 	file2 = open(file)
 	while 1:
 		line = file2.readline()
 		if not line: break
-		print line,
-		line.replace("", )
+#		print line,
+		line.replace(replace_str_array[0][1], replace_str_array[0][2])
+		file4.write(line)
 #		subprocess.call(["echo", file])
+	tmp_file.close()
+	file4.close()
 	file2.close()
 
 
