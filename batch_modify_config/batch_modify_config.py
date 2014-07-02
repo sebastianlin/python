434f859fbd3406c6a1c3c76b262331016c8e2e3f
colin <colin@realtek.com>
2014-07-02 10:10:38 +0800

    1. Support mulitiple tokens to be replaced. 2. Create temp file whose filename has prefix of "colin".

--- batch_modify_config/batch_modify_config.py
+++ batch_modify_config/batch_modify_config.py
@@ -3,21 +3,26 @@
 
 import os, sys, glob, subprocess, tempfile
 
-replace_str_array = [["CONFIG_CMDLINE_BOOL", "CONFIG_CMDLINE_BOOL=y", "# CONFIG_CMDLINE_BOOL is not set" ]]
+replace_str_array = [["CONFIG_CMDLINE_BOOL", "CONFIG_CMDLINE_BOOL=y", "# CONFIG_CMDLINE_BOOL is not set" ], ["CONFIG_PARPORT" , "# CONFIG_PARPORT is not set", "CONFIG_PARPORT=y"]]
 
-file4 = open(tempfile.mkstemp()[1], "rw")
+tempfile_tuple = tempfile.mkstemp(prefix="colin.")[1] 
+file4 = open(tempfile_tuple,  'w')
+print "The opened file is " + tempfile_tuple +"."
 
-
-for file in glob.glob('/home/mark/RTK_workshop/nike/svn/DTH_permission/nike_scpu/linux-2.6.34/config.develop.avhdd.nike.scpu.secure.nxx.nand.old'):
-	file2 = open(file)
+for file in glob.glob('/tmp/colin/test2/scpu/Linux/linux-2.6.34/config.compact.avhdd.nike.scpu.secure.nxx.nand.old'):
+	file2 = open(file, "rt")
 	while 1:
 		line = file2.readline()
 		if not line: break
 #		print line,
-		line.replace(replace_str_array[0][1], replace_str_array[0][2])
+		i = 0
+		while 1:
+			line=line.replace(replace_str_array[i][1], replace_str_array[i][2])
+			i = i+1
+			if i>=len(replace_str_array):
+				break
 		file4.write(line)
 #		subprocess.call(["echo", file])
-	tmp_file.close()
 	file4.close()
 	file2.close()
 
